c10f531b7d30c65f66a2d492048449c159a7aee0
Rune V. Sjøen <runesj@ifi.uio.no>
2012-05-14 18:35:50 +0200

    Updates to GOL

--- canvas-game-of-life/gol.js
+++ canvas-game-of-life/gol.js
@@ -2,37 +2,10 @@ var _canvas = document.getElementById("canvas");
 var _canvasBuffer = null;
 var _canvasContext = null;
 var _canvasBufferContext = null;
+var size = 20;
+var world = new World();
 
-var height 	= 9;
-var width  	= 36;
-var size 	= 20;
-var grid 	= Array(height * width);
-var grid_n	= Array(height * width);
-
-StateEnum = {
-	DEAD:  0,
-	ALIVE: 1,
-};
-
-ColorEnum = {
-
-};
-
-if (_canvas && _canvas.getContext) {
-	_canvasContext = _canvas.getContext('2d');
-	_canvasBuffer = document.createElement('canvas');
-	_canvasBuffer.width = _canvas.width;
-	_canvasBuffer.height = _canvas.height;
-	_canvasBufferContext = _canvasBuffer.getContext('2d');
-	initGrid();
-	setInterval(gameLoop, 1000);
-}
-
-var x = 10;
-var y = 10;
-
-function initGrid(){
-	var test = "\
+var test = "\
 000000000000000000000000100000000000\
 000000000000000000000010100000000000\
 000000000000110000001100000000000011\
@@ -42,32 +15,140 @@ function initGrid(){
 000000000010000010000000100000000000\
 000000000001000100000000000000000000\
 000000000000110000000000000000000000\
-		";
-	var tmp = test.split("");
-	for(var i = 0; i < width; i++) {
-		for(var j = 0; j < height; j++) {
-			if(tmp[j * width + i] == '1')
-				grid[j * width + i] = StateEnum.ALIVE;
-			else
-				grid[j * width + i] = StateEnum.DEAD;	
+";
+
+StateEnum = {
+	DEAD:  0,
+	ALIVE: 1,
+};
+
+if (_canvas && _canvas.getContext) {
+	_canvasBuffer 			= document.createElement('canvas');
+	_canvasBuffer.width 	= _canvas.width;
+	_canvasBuffer.height 	= _canvas.height;
+	_canvasContext 			= _canvas.getContext('2d');
+	_canvasBufferContext 	= _canvasBuffer.getContext('2d');
+	world.init(test);
+	setInterval(loop, 50);
+}
+
+function World() {
+	
+	this.worlds  = Array(2);
+	this.current = 0;
+	this.height  = 9;
+	this.width   = 36;
+	
+	this.init = function(str) {
+		this.worlds[0] = Array(this.height * this.width);
+		this.worlds[1] = Array(this.height * this.width);
+		var world = this.worlds[this.current];
+		var tmp = str.split("");
+		for(var i = 0; i < this.height; i++) {
+			for(var j = 0; j < this.width; j++) {
+				if(tmp[i * this.width + j] == 1) {
+					world[i * this.width + j] = StateEnum.ALIVE;
+				} else {
+					world[i * this.width + j] = StateEnum.DEAD;	
+				}
+			}
 		}
-	}
+	};
+	
+	this.getWorld = function() {
+		return this.worlds[this.current];
+	};
+	
+	this.updateCellState = function(x, y) {
+		var cnt = 0 ;
+		var grid = this.worlds[this.current];
+		var next = this.worlds[(this.current + 1) % 2]
+		
+		// Check the row above the current cell
+		if(y > 0){
+			// Top left
+			if(x > 0) {
+				if(grid[((y-1) * this.width) + (x-1)] == StateEnum.ALIVE) cnt++;			
+			}
+			// Top middle
+			if((grid[((y-1) * this.width) + x]) == StateEnum.ALIVE) cnt++;
+			// Top right
+			if(x < (this.width - 1)) {
+				if(grid[((y-1) * this.width) + (x+1)] == StateEnum.ALIVE) cnt++;	
+			}
+		}
+		
+		// Check the left side
+		if((x > 0) && (x < (this.width-1))) {
+			if(grid[(y * this.width) + (x-1)] == StateEnum.ALIVE) cnt++;
+		}
+		
+		// Check the right side
+		if(x < (this.width - 1)) {
+			if(grid[(y * this.width) + (x+1)] == StateEnum.ALIVE) cnt++;
+		}
+		
+		// Check the row below the current cell
+		if(y < (this.height - 1)){
+			if(x>0) {
+				if(grid[((y+1) * this.width) + (x-1)] == StateEnum.ALIVE) cnt++;
+			}
+			
+			if(grid[((y+1) * this.width) + x] == StateEnum.ALIVE) cnt++;
+			
+			if(x < (this.width - 1)) {
+				if(grid[((y+1) * this.width) + (x+1)] == StateEnum.ALIVE) cnt++;
+			}
+		}
+
+		switch(cnt) {
+			case 0:
+			case 1:
+				next[(y * this.width) + x] = StateEnum.DEAD;
+				break;
+			case 2:
+			case 3:
+				if(grid[(y * this.width) + x] == StateEnum.DEAD && cnt == 3) {
+					next[(y * this.width) + x] = StateEnum.ALIVE;
+				} else {
+					next[(y * this.width) + x] = grid[(y * this.width) + x];
+				}
+				break;
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+				next[(y * this.width) + x] = StateEnum.DEAD;
+				break;
+		}
+	};
+	
+	this.update = function() {
+		for(var i = 0; i < this.height; i++) {
+			for(var j = 0; j < this.width; j++) {
+				this.updateCellState(j, i);
+			}
+		}
+		this.current = (this.current + 1) % 2;
+	};
 }
 
 function clearCanvas() {
-	_canvasContext.clearRect(0, 0, width * size, height * size);
-	_canvasBufferContext.clearRect(0, 0, width * size, height * size);
+	_canvasContext.clearRect(0, 0, world.width * size, world.height * size);
+	_canvasBufferContext.clearRect(0, 0, world.width * size, world.height * size);
 }
 
 function drawGrid() {
 	var size = 20;
+	var grid = world.getWorld();
 	var display = _canvasBufferContext;
 	display.fillStyle = "#000";
-	display.fillRect(0,0, width * size, height * size);
+	display.fillRect(0,0, world.width * size, world.height * size);
 	display.strokeStyle = "#666";
-	for(var i = 0; i < width; i++) {
-		for(var j = 0; j < height; j++) {
-			if(grid[j * width + i] == StateEnum.ALIVE) {
+	for(var i = 0; i < world.width; i++) {
+		for(var j = 0; j < world.height; j++) {
+			if(grid[j * world.width + i] == StateEnum.ALIVE) {
 				display.fillStyle = "#F00";
 				display.fillRect(i * size, j * size, size, size);
 			} else {
@@ -77,56 +158,9 @@ function drawGrid() {
 	}
 }
 
-function getCellState(x, y) {
-	var cnt = 0 ;
-	//Loop through world counting cells
-	if(y > 0){
-		if(x > 0)
-			if(grid[y-1][x-1] == StateEnum.ALIVE) cnt++;
-			if(grid[y-1][x] == StateEnum.ALIVE) cnt++;
-		if(x<(width-1))
-			if(grid[y-1][x+1] == StateEnum.ALIVE) cnt++;
-	}
-
-	if((x > 0) && (x < (width-1)))
-		if(grid[y][x-1] == StateEnum.ALIVE) cnt++;
-	if(x < (width-1))
-		if(grid[y][x+1] == StateEnum.ALIVE) cnt++;
-
-	if(y<(height-1)){
-		if(x>0)
-			if(grid[y+1][x-1] == StateEnum.ALIVE) cnt++;
-			if(grid[y+1][x] == StateEnum.ALIVE) cnt++;
-		if(x<(width-1))
-			if(grid[y+1][x+1] == StateEnum.ALIVE) cnt++;
-	}
-
-//	state->neighbors = cnt;
-//	state->status    = cworld[y][x];
-
-  return cnt;
-}
-
-function updateWorld() {
-	for(var i = 0; i < width; i++) {
-		for(var j = 0; j < height; j++) {
-			state = getCellState(i, j);
-			
-		}
-	}
-}
-	
-function gameLoop() {
+function loop() {
 	clearCanvas();
 	drawGrid();
-	updateWorld();
-	
-	/*
-	_canvasBufferContext.fillStyle = "rgb(127,0,0)";
-	_canvasBufferContext.fillRect(x, y, 20, 20);
-	x += 20;
-	y += 20;
-	*/
-	
+	world.update();
 	_canvasContext.drawImage(_canvasBuffer, 0, 0);
 }
