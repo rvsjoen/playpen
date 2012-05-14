be8e05df7089a35c042caafade581713bc8e8029
Rune V. Sjøen <runesj@ifi.uio.no>
2012-05-14 20:15:26 +0200

    Add color codes and start on UI

--- canvas-game-of-life/gol.js
+++ canvas-game-of-life/gol.js
@@ -2,8 +2,9 @@ var _canvas = document.getElementById("canvas");
 var _canvasBuffer = null;
 var _canvasContext = null;
 var _canvasBufferContext = null;
-var size = 20;
-var world = new World();
+var world = null;
+var timer = null;
+var size = 15;
 
 var test = "\
 00000000000000000000000000000000000000\
@@ -35,11 +36,18 @@ StateEnum = {
 };
 
 ColorEnum = {
+	NONE:			"#000",
 	UNDERPOPULATED: "#F00",
-	SUSTAINED: 		"#FF0",
-	OVERCROWDED: 	"#00F",
+	SUSTAINED: 		"#00F",
+	OVERCROWDED: 	"#0FF",
 	REPRODUCED: 	"#0F0",
 };
+
+function Cell() {
+	this.neighbors = 0;
+	this.status    = 0;
+	this.color	   = 0;
+}
 
 if (_canvas && _canvas.getContext) {
     _canvasBuffer = document.createElement('canvas');
@@ -47,8 +55,6 @@ if (_canvas && _canvas.getContext) {
     _canvasBuffer.height = _canvas.height;
     _canvasContext = _canvas.getContext('2d');
     _canvasBufferContext = _canvasBuffer.getContext('2d');
-    world.init(test);
-    setInterval(loop, 50);
 }
 
 function World() {
@@ -61,15 +67,30 @@ function World() {
     this.init = function(str) {
         this.worlds[0] = Array(this.height * this.width);
         this.worlds[1] = Array(this.height * this.width);
-        var world = this.worlds[this.current];
         var tmp = str.split("");
+		
+		// Initialize the first world
+		var world = this.worlds[this.current];
         for(var i = 0; i < this.height; i++) {
             for(var j = 0; j < this.width; j++) {
+				c = new Cell();
                 if(tmp[i * this.width + j] == 1) {
-                    world[i * this.width + j] = StateEnum.ALIVE;
+					c.state = StateEnum.ALIVE;
                 } else {
-                    world[i * this.width + j] = StateEnum.DEAD;    
+					c.state = StateEnum.DEAD;
                 }
+				c.color = ColorEnum.NONE;
+				world[i * this.width + j] = c;
+            }
+        }
+		
+		// Initialize the second world
+		var world = this.worlds[this.current + 1 % 2];
+        for(var i = 0; i < this.height; i++) {
+            for(var j = 0; j < this.width; j++) {
+				c = new Cell();
+				c.color = ColorEnum.NONE;
+				world[i * this.width + j] = c;
             }
         }
     };
@@ -82,57 +103,62 @@ function World() {
         var cnt = 0 ;
         var grid = this.worlds[this.current];
         var next = this.worlds[(this.current + 1) % 2]
-        
+ 
         // Check the row above the current cell
         if(y > 0){
             // Top left
             if(x > 0) {
-                if(grid[((y-1) * this.width) + (x-1)] == StateEnum.ALIVE) cnt++;            
+                if(grid[((y-1) * this.width) + (x-1)].state == StateEnum.ALIVE) cnt++;            
             }
             
             // Top middle
-            if((grid[((y-1) * this.width) + x]) == StateEnum.ALIVE) cnt++;
+            if(grid[((y-1) * this.width) + x].state == StateEnum.ALIVE) cnt++;
             
             // Top right
             if(x < (this.width - 1)) {
-                if(grid[((y-1) * this.width) + (x+1)] == StateEnum.ALIVE) cnt++;    
+                if(grid[((y-1) * this.width) + (x+1)].state == StateEnum.ALIVE) cnt++;    
             }
         }
         
         // Check the left side
         if((x > 0) && (x < (this.width-1))) {
-            if(grid[(y * this.width) + (x-1)] == StateEnum.ALIVE) cnt++;
+            if(grid[(y * this.width) + (x-1)].state == StateEnum.ALIVE) cnt++;
         }
         
         // Check the right side
         if(x < (this.width - 1)) {
-            if(grid[(y * this.width) + (x+1)] == StateEnum.ALIVE) cnt++;
+            if(grid[(y * this.width) + (x+1)].state == StateEnum.ALIVE) cnt++;
         }
         
         // Check the row below the current cell
         if(y < (this.height - 1)){
             if(x>0) {
-                if(grid[((y+1) * this.width) + (x-1)] == StateEnum.ALIVE) cnt++;
+                if(grid[((y+1) * this.width) + (x-1)].state == StateEnum.ALIVE) cnt++;
             }
             
-            if(grid[((y+1) * this.width) + x] == StateEnum.ALIVE) cnt++;
+            if(grid[((y+1) * this.width) + x].state == StateEnum.ALIVE) cnt++;
             
             if(x < (this.width - 1)) {
-                if(grid[((y+1) * this.width) + (x+1)] == StateEnum.ALIVE) cnt++;
+                if(grid[((y+1) * this.width) + (x+1)].state == StateEnum.ALIVE) cnt++;
             }
         }
 
+		next[(y * this.width) + x].color = ColorEnum.NONE;
         switch(cnt) {
             case 0:
             case 1:
-                next[(y * this.width) + x] = StateEnum.DEAD;
+                next[(y * this.width) + x].state = StateEnum.DEAD;
+				if(next[(y * this.width) + x].state != grid[(y * this.width) + x].state)
+					next[(y * this.width) + x].color = ColorEnum.UNDERPOPULATED;
                 break;
             case 2:
             case 3:
-                if(grid[(y * this.width) + x] == StateEnum.DEAD && cnt == 3) {
-                    next[(y * this.width) + x] = StateEnum.ALIVE;
+                if(grid[(y * this.width) + x].state == StateEnum.DEAD && cnt == 3) {
+                    next[(y * this.width) + x].state = StateEnum.ALIVE;
+					next[(y * this.width) + x].color = ColorEnum.REPRODUCED;
                 } else {
-                    next[(y * this.width) + x] = grid[(y * this.width) + x];
+                    next[(y * this.width) + x].state = grid[(y * this.width) + x].state;
+					next[(y * this.width) + x].color = ColorEnum.SUSTAINED;
                 }
                 break;
             case 4:
@@ -140,9 +166,11 @@ function World() {
             case 6:
             case 7:
             case 8:
-                next[(y * this.width) + x] = StateEnum.DEAD;
+                next[(y * this.width) + x].state = StateEnum.DEAD;
+				next[(y * this.width) + x].color = ColorEnum.OVERCROWDED;
                 break;
         }
+		next[(y * this.width) + x].neighbors = cnt;
     };
     
     this.update = function() {
@@ -161,20 +189,16 @@ function clearCanvas() {
 }
 
 function drawGrid() {
-    var size = 20;
     var grid = world.getWorld();
     var display = _canvasBufferContext;
     display.fillStyle = "#000";
     display.fillRect(0,0, world.width * size, world.height * size);
     display.strokeStyle = "#666";
     for(var i = 0; i < world.width; i++) {
-        for(var j = 0; j < world.height; j++) {
-            if(grid[j * world.width + i] == StateEnum.ALIVE) {
-                display.fillStyle = "#F00";
-                display.fillRect(i * size, j * size, size, size);
-            } else {
-                display.strokeRect(i * size, j * size, size, size);
-            }
+		for(var j = 0; j < world.height; j++) {
+			display.fillStyle = grid[j * world.width + i].color;
+            display.fillRect(i * size, j * size, size, size);
+			display.strokeRect(i * size, j * size, size, size);
         }
     }
 }
@@ -185,3 +209,28 @@ function loop() {
     world.update();
     _canvasContext.drawImage(_canvasBuffer, 0, 0);
 }
+
+// Now on to the UI stuff
+$(document).ready(function() {
+	$("#btnStart").click(function() {
+		$(this).attr('value', 'Restart');
+		world = new World();
+		world.init(test);
+		if(timer != null) {
+			clearInterval(timer);
+		}
+		timer = setInterval(loop, 50);
+	});
+	
+	$("#sliderSize").slider(
+		{min: 5, max: 25}
+	);
+	
+	$("#sliderHeight").slider(
+		{min: 10, max: 50}
+	);
+	
+	$("#sliderWidth").slider(
+		{min: 10, max: 50}
+	);
+});
